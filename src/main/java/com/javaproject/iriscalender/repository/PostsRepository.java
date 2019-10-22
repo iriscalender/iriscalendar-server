63cb4ec16d3084c4d8ec97fcda9dafee69bc1487
dbswo8910 <miraedbswo@amazeme.live>
2019-10-22 11:24:31 +0900

    <delete> base file

--- src/main/java/com/javaproject/iriscalender/repository/PostsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javaproject.iriscalender.repository;
-
-import com.javaproject.iriscalender.domain.Posts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-// DB Layer 접근자
-public interface PostsRepository extends JpaRepository<Posts, Long>{
-}
