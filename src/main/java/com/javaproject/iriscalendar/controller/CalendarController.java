c2c17c7d4cdee10ed7e442ef85d6367b1a863547
dbswo8910 <miraedbswo@amazeme.live>
2019-11-18 22:52:50 +0900

    fix travis test

--- src/main/java/com/javaproject/iriscalendar/controller/CalendarController.java
+++ src/main/java/com/javaproject/iriscalendar/controller/CalendarController.java
@@ -1,4 +1,17 @@
 package com.javaproject.iriscalendar.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("calendar")
 public class CalendarController {
+//    public ? addAutomaticCalendar(@RestController("Authorization") String auth, @RequestBody @Valid AutomaticCalendarParam automaticCalendarParam) {
+//
+//    }
+//    public ? calendar(@RequestHeader("Authorization") String auth, @PathVariable Integer id) {
+//
+//    }
+
 }
