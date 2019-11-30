31156bd0309a082f0e1f73d4ed10920bd4933e02
dbswo8910 <miraedbswo@amazeme.live>
2019-11-30 17:20:31 +0900

    add manual calendar entity

---
+++ src/main/java/com/javaproject/iriscalendar/model/entity/ManualCalendar.java
@@ -0,0 +1,40 @@
+package com.javaproject.iriscalendar.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter @Setter
+@Entity
+@Builder
+@DynamicInsert
+@DynamicUpdate
+@Table(name="manual_calendar")
+public class ManualCalendar {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @JsonProperty("id")
+    private Long idx;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @Embedded
+    @JsonIgnore
+    private User user;
+
+    @NotNull
+    private String category;
+    @NotNull
+    private String calendarName;
+    @NotNull
+    private String startTime;
+    @NotNull
+    private String endTime;
+}
+
