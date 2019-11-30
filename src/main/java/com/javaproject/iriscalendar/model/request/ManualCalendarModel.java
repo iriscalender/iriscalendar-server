3751182ce4c6303686013799ef20ae198ffbaf69
dbswo8910 <miraedbswo@amazeme.live>
2019-11-30 17:20:40 +0900

    add manual calendar request model

---
+++ src/main/java/com/javaproject/iriscalendar/model/request/ManualCalendarModel.java
@@ -0,0 +1,23 @@
+package com.javaproject.iriscalendar.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class ManualCalendarModel {
+    @NotNull
+    private String category;
+    @NotNull
+    private String calendarName;
+    @NotNull
+    private String startTime;
+    @NotNull
+    private String endTime;
+}
