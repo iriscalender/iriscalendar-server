fbea9be6c91f127b4bc01d10dcec866d088c8fc7
dbswo8910 <miraedbswo@amazeme.live>
2019-11-30 17:20:50 +0900

    add manual calendar request repository

---
+++ src/main/java/com/javaproject/iriscalendar/repository/ManualCalendarRepository.java
@@ -0,0 +1,16 @@
+package com.javaproject.iriscalendar.repository;
+
+import com.javaproject.iriscalendar.model.entity.ManualCalendar;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ManualCalendarRepository extends JpaRepository<ManualCalendar, String> {
+    Optional<ManualCalendar> findManualCalendarByUserId(String id);
+    Optional<ArrayList<ManualCalendar>> findAllByUserIdOrderByStartTimeAsc(String id);
+    Optional<ManualCalendar> findManualCalendarByIdx(Long idx);
+}
